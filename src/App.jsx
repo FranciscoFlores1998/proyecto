8d447cdf0b9a84cc892f9a86011326808d407946
FranciscoFlores1998 <francisco.flores1601@alumnos.ubiobio.cl>
2024-10-15 01:22:28 -0300

    CDN icons + Limpieza css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import { Cabecera } from "./componentes/cabecera";
 import '../src/style.css';
+import { Page } from "./componentes/page";
 
 export function App() {
 
     return (
         <Fragment>
-            <Cabecera><h1> </h1></Cabecera>
-
+            <Cabecera></Cabecera>
+            <Page></Page>
         </Fragment>
     );
 }
---
+++ src/componentes/page.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+export function Page() {
+    return (
+        <Fragment>
+            <div className="container">
+            </div>
+        </Fragment>
+    )
+}
